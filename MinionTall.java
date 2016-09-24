cf4f9fb31dc39d08e720ddb6888aa189a0d1a949
Jiheng Lu <jihenglu@hotmail.com>
2016-09-24 16:53:37 -0700

    Revision on MinionTall
    Make changes to the file and description. Specify the job of MinionTall

---
+++ MinionTall.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * This is the tall minion, it will pick the numbers higher than the pivot.
+ * 
+ * @author Jiheng Lu
+ * @version 9/24/2016
+ */
+public class MinionTall extends Minions
+{
+    /**
+     * Act - do whatever the MinionTall wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
